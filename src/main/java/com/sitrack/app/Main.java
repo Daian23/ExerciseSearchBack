fabfa231f9a149c5f2a9c64c4e015e7812808ab2
Daian23 <daiana23@gmail.com>
2022-10-31 04:27:10 -0300

    update files

--- src/main/java/com/sitrack/app/Main.java
+++ src/main/java/com/sitrack/app/Main.java
@@ -2,10 +2,6 @@ package com.sitrack.app;
 
 import com.sitrack.app.controller.Controller;
 
-/**
- * Hello world!
- *
- */
 public class Main
 {
     public static void main( String[] args ) throws Exception {
